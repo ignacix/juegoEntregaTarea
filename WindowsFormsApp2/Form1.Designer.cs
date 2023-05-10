7b7d14b31e40b8532f19084fcebd2118c7a1a7d0
igna <ignanj8@gmail.com>
2023-05-10 15:41:29 -0300

    Agregar archivos de proyecto.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Image = global::WindowsFormsApp2.Properties.Resources.gota;
+            this.pictureBox1.Location = new System.Drawing.Point(79, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(134, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Puntos:";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Enabled = false;
+            this.pictureBox2.Image = global::WindowsFormsApp2.Properties.Resources.hoja;
+            this.pictureBox2.Location = new System.Drawing.Point(79, 374);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(254, 461);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximumSize = new System.Drawing.Size(270, 500);
+            this.MinimumSize = new System.Drawing.Size(270, 500);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int[] vector = {0,79,158};
+        public Random random = new Random();
+        int cont = 1;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();                                                                                  
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //movimiento Gota
+            int x = pictureBox1.Location.X;
+            int y = pictureBox1.Location.Y;
+            y=y+5;
+            
+            pictureBox1.Location = new Point(x,y);                        
+
+            //Utilizo la misma gota(pictureBox1)
+            if (pictureBox1.Location.Y > 500)
+            {
+                //creo el obj random
+                //saco la longitud del vector. Creo un numero aleatorio y lo utilizo para acceder a algun numero del vector
+                int aleatorio = random.Next(0, vector.Length);
+                pictureBox1.Location = new Point(vector[aleatorio], 0);
+                label1.Text = $"Puntos: {cont++}";
+            }
+
+            if ((pictureBox1.Location.Y == 290) && (pictureBox1.Location.X == pictureBox2.Location.X))
+            {
+                timer1.Stop();
+                var jugar = MessageBox.Show($"\tPERDISTE\n\nPuntos: {cont-1}\n\n¿Quiere jugar denuevo?","",MessageBoxButtons.YesNo);                
+                if (jugar.ToString()=="Yes")
+                {
+                    cont = 0;
+                    timer1.Start();
+                }
+                else
+                {
+                    Form.ActiveForm.Close();
+                }
+            }                                
+            
+                
+                        
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            int x = pictureBox2.Location.X;
+            int y = pictureBox2.Location.Y;
+            MouseEventArgs click = (MouseEventArgs)e;
+            if (click.Button==MouseButtons.Left)
+            {
+                if ((x-79)>=0)                
+                    pictureBox2.Location = new Point(x-79,y);
+                
+            }
+            else if (click.Button == MouseButtons.Right)
+            {
+                if ((x+79)<=200)
+                    pictureBox2.Location = new Point(x+79,y);
+            }
+            //label1.Text = (pictureBox2.Location.X.ToString());
+        }
+    }
+}
